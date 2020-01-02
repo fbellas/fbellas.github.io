21af201e09e5451d1ac0dbd5b9ead42c30f14972
Fernando <fbellas@udc.es>
2020-01-02 17:48:03 +0100

    Nueva versión creada a partir de una versión *inicial* de pa-shop 1.1.0.

---
+++ precache-manifest.985eb2c9b93dabc1e2fc8d40fab1d344.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26163940a554f02b571451131809b3df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33fc854a4f3ed3002013",
+    "url": "/static/css/2.900b2a8e.chunk.css"
+  },
+  {
+    "revision": "becd1422e22e26584184",
+    "url": "/static/css/main.bf1aa0d3.chunk.css"
+  },
+  {
+    "revision": "33fc854a4f3ed3002013",
+    "url": "/static/js/2.6f2420dc.chunk.js"
+  },
+  {
+    "revision": "e124124f096e36d350c0353f2c6d8998",
+    "url": "/static/js/2.6f2420dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "becd1422e22e26584184",
+    "url": "/static/js/main.e3e34fd2.chunk.js"
+  },
+  {
+    "revision": "6b7f0226cdd203db3ffa",
+    "url": "/static/js/runtime-main.68aaaf01.js"
+  },
+  {
+    "revision": "140f41485edce6f713abe17625eba4c1",
+    "url": "/static/media/fa-solid-900.140f4148.woff"
+  },
+  {
+    "revision": "4910ec733558f59bf05834d4f831a48d",
+    "url": "/static/media/fa-solid-900.4910ec73.eot"
+  },
+  {
+    "revision": "813b8aee60f235b36887a388b70e1359",
+    "url": "/static/media/fa-solid-900.813b8aee.woff2"
+  },
+  {
+    "revision": "e57e108a1ae04ca2b27cab75e4478867",
+    "url": "/static/media/fa-solid-900.e57e108a.ttf"
+  },
+  {
+    "revision": "e98a92ac980c63c46a7e25c4fbcf30ef",
+    "url": "/static/media/fa-solid-900.e98a92ac.svg"
+  }
+]);
